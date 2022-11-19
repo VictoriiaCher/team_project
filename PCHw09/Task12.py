b28b3add93d4ac088ef87e962d1642e1f1ef7e3c
VOliinyk1 <vov.25.98@gmail.com>
2022-11-19 14:13:14 +0200

    Update Task12.py

--- PCHw09/Task12.py
+++ PCHw09/Task12.py
@@ -2,5 +2,4 @@ from functools import reduce
 
 
 def amount_payment(payment):
-    return reduce(lambda x, y: x + y, [i for i in payment if i > 0])
-    #return reduce(lambda x , y: x + y, filter( lambda x: x > 0, payment))
+    return reduce(lambda x, y: x + y , [i for i in filter(lambda x : x >= 0, payment)])
